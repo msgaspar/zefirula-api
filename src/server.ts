96e409e7dd071844be4a1f8a8bfa06f2dfe139fa
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-06 17:41:05 -0300

    add eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello, world' })
-})
+app.get('/', (request, response) => response.json({ message: 'Hello, world' }));
 
 app.listen(3333);
