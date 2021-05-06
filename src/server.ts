49144de6e663415738caa6a92b92736be9d688ff
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-06 18:07:14 -0300

    config debugger

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,9 @@ app.get('/', (request, response) => {
   return response.json({ message: 'Hello, world' });
 });
 
+app.post('/clubs', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
+
 app.listen(3333, () => console.log('Server is running...'));
