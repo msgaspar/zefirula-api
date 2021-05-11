575b7edc4dd6520cb782f5d62e82f95de6562c1f
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-11 14:25:48 -0300

    pass userId to request after authentication middleware

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -31,6 +31,10 @@ async function ensureAuthenticated(
       throw new AppError('User does not exist', 401);
     }
 
+    request.user = {
+      id: userId,
+    };
+
     next();
   } catch {
     throw new AppError('Invalid token', 401);
