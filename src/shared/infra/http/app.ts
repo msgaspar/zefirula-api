10ef298cb66d26e3198f3bb8428d8204712d2794
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-12 19:10:14 -0300

    remove swagger

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
+import createConnection from '@shared/infra/typeorm';
+
+import '../../container';
+
+import { AppError } from '../../errors/AppError';
+import { router } from './routes';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,36 +1,3 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
-
-import createConnection from '@shared/infra/typeorm';
-
-import '../../container';
-
-import swaggerFile from '../../../config/swagger.json';
-import { AppError } from '../../errors/AppError';
-import { router } from './routes';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message,
-    });
-  }
-
-  return response.status(500).json({
-    status: 'error',
-    message: `Internal server error - ${err.message}`,
-  });
-});
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server is running...'));
