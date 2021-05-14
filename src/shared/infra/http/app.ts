e0a759c02eccedbd1ca1bc4b72065046b672ee7c
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-14 12:20:18 -0300

    install cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
